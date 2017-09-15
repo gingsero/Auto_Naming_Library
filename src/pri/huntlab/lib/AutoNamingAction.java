d8829af9e6112a73d2e5718996fd9ec1b3cb6727
master <kim.dhdbr@gmail.com>
2017-09-15 23:32:30 +0900

    First commit

---
+++ src/pri/huntlab/lib/AutoNamingAction.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package pri.huntlab.lib;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author dhkim
+ * Description Make Class & Method name by auto system
+ * @since 2017.09.15
+ * @version 0.1
+ */
+public class AutoNamingAction {
+	
+	private String sourceClassName;
+	
+	private String sourcePackageName;
+	
+	private String sourceActionName;
+	
+	private String[] arrayClassName;
+	
+	private String[] arrayMethodName;
+	
+	
+	public AutoNamingAction(String cName, String pName, String aName){
+		this.sourceClassName = cName;
+		this.sourcePackageName = pName;
+		this.sourceActionName = aName;
+	}
+	
+	
+	
+	
+}
